b03cd53b71085b3c9daba62b59e9026b4d5bb740
WLBRT <wilbert.alejandromtz@gmail.com>
2018-01-24 08:41:34 -0600

    Ejemplo
    Tengo una importante practica, donde con bontones puedo cambiar de color al titulo.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function azul(){
+	$("#var").css("color", "blue")
+	//$("#var").css("color", "black")
+
+}
+
+function verde(){
+	$("#var").css("color", "green")
+
+	//$("#var").css("color", "black")
+
+}
+
+function morado(){
+	$("#var").css("color", "purple")
+
+	//$("#var").css("color", "black")
+
+}
+
+function Fondo(){$("#hdos").css({background:"blue"})}
